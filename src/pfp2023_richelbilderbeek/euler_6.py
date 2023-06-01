4ae6d90764995f5dd857cf53c4044760b7976b4d
acorbat <agustin.corbat@ki.se>
2023-06-01 14:20:15 +0200

    Add documentation for 'sum_square_differences'

--- src/pfp2023_richelbilderbeek/euler_6.py
+++ src/pfp2023_richelbilderbeek/euler_6.py
@@ -1,2 +1,14 @@
-def sum_square_differences():
+def sum_square_differences(n):
+    """Calculates the difference between the sum of squares of numbers and the
+    square of the sum of numbers
+
+    Parameters
+    ----------
+    n: int
+        Highest number to consider
+
+    Returns
+    -------
+    : int
+        Returns the difference"""
     pass

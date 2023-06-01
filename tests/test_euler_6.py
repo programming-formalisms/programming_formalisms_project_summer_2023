3c79e681f8fb9585b5e9c7b13780b3be0aedc16f
acorbat <agustin.corbat@ki.se>
2023-06-01 14:20:48 +0200

    Add test for raising TypeError in 'sum_square_differences'

--- tests/test_euler_6.py
+++ tests/test_euler_6.py
@@ -2,8 +2,10 @@ import unittest
 
 from src.pfp2023_richelbilderbeek.euler_6 import sum_square_differences
 
+
 class TestEasySolutions(unittest.TestCase):
     def test_sum_square_differences(self):
          """Test 'sum_square_differences'."""
          self.assertIsNotNone(sum_square_differences.__doc__)
+         self.assertRaises(TypeError, sum_square_differences, 10.5)
          

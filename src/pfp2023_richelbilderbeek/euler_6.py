b6ed3c6fdd6a9b296aee944cd88f0a2a2611fc56
Carolina Nilsson <carolina.nilsson@smhi.se>
2023-06-01 14:14:03 +0200

    Added Euler 6 and first test

---
+++ src/pfp2023_richelbilderbeek/euler_6.py
@@ -0,0 +1,2 @@
+def sum_square_differences():
+    pass
---
+++ tests/test_euler_6.py
@@ -0,0 +1,9 @@
+import unittest
+
+from src.pfp2023_richelbilderbeek.euler_6 import sum_square_differences
+
+class TestEasySolutions(unittest.TestCase):
+    def test_sum_square_differences(self):
+         """Test 'sum_square_differences'."""
+         self.assertIsNotNone(sum_square_differences.__doc__)
+         

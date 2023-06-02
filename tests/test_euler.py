9acfcf5f100a6b2289ddd5bab5ca8b8bfc9e9738
RenateWilcke <renate.wilcke@smhi.se>
2023-06-02 09:27:13 +0200

    fsdv

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -11,8 +11,7 @@ class TestEasySolutions(unittest.TestCase):
     """Class to test the functions in
     src.pfp2023_richelbilderbeek.easy_solutions euler 1 problem.
     """
-
-
+    
     def test_euler1(self):
         """Test Euler 1 problem: multiples of 3 or 5."""
         self.assertIsNotNone(euler_1.__doc__)

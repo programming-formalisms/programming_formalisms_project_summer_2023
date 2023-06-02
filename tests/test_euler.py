f4a0c26a64d3d3a0d68b580ba35224313a086875
RenateWilcke <renate.wilcke@smhi.se>
2023-06-02 09:11:02 +0200

    fixed more ruff style issues

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -5,9 +5,12 @@ from src.pfp2023_richelbilderbeek.euler import (
 )
 
 class TestEasySolutions(unittest.TestCase):
-    """Class to test the functions in src.pfp2023_richelbilderbeek.easy_solutions euler 1 problem."""
+    """Class to test the functions in 
+    src.pfp2023_richelbilderbeek.easy_solutions euler 1 problem.
+    """
 
     def test_euler1(self):
         """Test Euler 1 problem: multiples of 3 or 5."""
-        self.assertIsNotNone(euler_1.__doc__)        
-        self.assertTrue(euler_1() == 233168)
+        self.assertIsNotNone(euler_1.__doc__)
+        result = 233168
+        self.assertTrue(euler_1() == result)
